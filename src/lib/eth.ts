f680aab05ffb0727fef10501483dd5224f7dd850
Connor Mendenhall <cmendenhall@8thlight.com>
2021-08-10 22:52:56 -0400

    Add web3modal providers

--- src/lib/eth.ts
+++ src/lib/eth.ts
@@ -1,9 +1,40 @@
 import { Web3Provider } from "@ethersproject/providers";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { WalletLink } from "walletlink";
 import Web3Modal from "web3modal";
+import coinbaseLogo from "../img/coinbase-wallet.svg";
 
 const web3Modal = new Web3Modal({
   network: "mainnet",
-  cacheProvider: true,
+  cacheProvider: false,
+  providerOptions: {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: { infuraId: "4ea3d70e787e400cbf91e18dcd8796d1" },
+    },
+    "custom-walletlink": {
+      display: {
+        logo: coinbaseLogo,
+        name: "WalletLink",
+        description: "Scan with WalletLink to connect",
+      },
+      options: {
+        appName: "Doxx",
+        networkUrl: `https://mainnet.infura.io/v3/4ea3d70e787e400cbf91e18dcd8796d1`,
+        chainId: 1,
+      },
+      package: WalletLink,
+      connector: async (_, options) => {
+        const { appName, networkUrl, chainId } = options;
+        const walletLink = new WalletLink({
+          appName,
+        });
+        const provider = walletLink.makeWeb3Provider(networkUrl, chainId);
+        await provider.enable();
+        return provider;
+      },
+    },
+  },
 });
 
 const loadProvider = async () => {
